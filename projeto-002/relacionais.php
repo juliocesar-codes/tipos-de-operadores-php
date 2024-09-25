6f08e34ecb0d94d74aeb40cb25bd2232e88e8c65
julio-frontend <cesar-pb@hotmail.com>
2024-09-25 15:16:25 -0300

    Adicionando os Operadores Relacionais

--- projeto-002/relacionais.php
+++ projeto-002/relacionais.php
@@ -1,9 +1,10 @@
 <?php
 echo "<h1>Operadores Relacionais</h1>";
-$valorA = 10;
+$valorA = 15;
 $valorB = 5;
 $valorC = 10;
 echo "Valores de referência: A=10 B=5 C=10";
+
 echo "<br><hr><br>";
 echo "<h3>Igualdades: == </h3>";
 if($valorA == $valorB){
@@ -12,3 +13,48 @@ if($valorA == $valorB){
     $igual = 'falso';
 }
 echo "Na comparação: {$valorA} == {$valorB} o resultado é {$igual}";
+
+echo "<br><hr><br>";
+echo "<h3>Maior que: > </h3>";
+if($valorA > $valorB){
+    $igual = 'verdadeiro';
+}else{
+    $igual = 'falso';
+}
+echo "Na comparação: {$valorA} > {$valorB} o resultado é {$igual}";
+
+echo "<br><hr><br>";
+echo "<h3>Menor que: < </h3>";
+if($valorA < $valorB){
+    $igual = 'verdadeiro';
+}else{
+    $igual = 'falso';
+}
+echo "Na comparação: {$valorA} < {$valorB} o resultado é {$igual}";
+
+echo "<br><hr><br>";
+echo "<h3>Maior ou igual:>= </h3>";
+if($valorA >= $valorB){
+    $igual = 'verdadeiro';
+}else{
+    $igual = 'falso';
+}
+echo "Na comparação: {$valorA} >= {$valorB} o resultado é {$igual}";
+
+echo "<br><hr><br>";
+echo "<h3>Menor ou igual: <= </h3>";
+if($valorA <= $valorB){
+    $igual = 'verdadeiro';
+}else{
+    $igual = 'falso';
+}
+echo "Na comparação: {$valorA} <= {$valorB} o resultado é {$igual}";
+
+echo "<br><hr><br>";
+echo "<h3>Diferente de: !=</h3>";
+if($valorA != $valorB){
+    $igual = 'verdadeiro';
+}else{
+    $igual = 'falso';
+}
+echo "Na comparação: {$valorA} != {$valorB} o resultado é {$igual}";
